4b77f82b195b35860d350042e6a8f11f363a728e
akvilary <akvilary@gmail.com>
2026-02-27 09:44:35 +0800

    Add niv formula v0.1.0

---
+++ Formula/niv.rb
@@ -0,0 +1,18 @@
+class Niv < Formula
+  desc "Vim-like terminal text editor with LSP and Tree-sitter support"
+  homepage "https://github.com/akvilary/niv"
+  url "https://github.com/akvilary/niv/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "1528f3d3481a1df5ea19c8aec8be2bf1be46d69ab88bcfc5daf684ff7e637373"
+  license "MIT"
+
+  depends_on "nim" => :build
+
+  def install
+    system "nim", "c", "-d:release", "--opt:size", "src/niv.nim"
+    bin.install "niv"
+  end
+
+  test do
+    assert_predicate bin/"niv", :executable?
+  end
+end
