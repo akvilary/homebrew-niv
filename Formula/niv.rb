eeb22be46b6d6ea1d0a67c9477775c089d52fd86
akvilary <akvilary@gmail.com>
2026-02-28 00:07:30 +0800

    update to v0.4.0: remove tree-sitter, add all bundled LSP servers

--- Formula/niv.rb
+++ Formula/niv.rb
@@ -1,32 +1,38 @@
 class Niv < Formula
-  desc "Vim-like terminal text editor with LSP and Tree-sitter support"
+  desc "Vim-like terminal text editor with bundled LSP servers"
   homepage "https://github.com/akvilary/niv"
   url "https://github.com/akvilary/niv/archive/refs/heads/main.tar.gz"
-  sha256 "fd957a4aa06a32ca01174e42395323afd492c5abe3a8809cb8715462772af0a7"
-  version "0.3.0"
+  sha256 "233c60b1b8e064afb1cd67940954563db54cb556a4898b39eacaef069caf04c4"
+  version "0.4.0"
   license "MIT"
 
   depends_on "gcc" => :build
   depends_on "nim" => :build
 
-  resource "tree-sitter" do
-    url "https://github.com/tree-sitter/tree-sitter/archive/refs/tags/v0.26.6.tar.gz"
-    sha256 "b4218185a48a791d4022ab3969709e271a70a0253e94792abbcf18d7fcf4291c"
-  end
-
   def install
-    (buildpath/"deps/tree-sitter-0.26.6").mkpath
-    resource("tree-sitter").stage do
-      (buildpath/"deps/tree-sitter-0.26.6").install Dir["*"]
-    end
-
     system "nimble", "build", "-y", "-d:release"
     bin.install buildpath/"niv"
     bin.install buildpath/"niv_json_lsp"
+    bin.install buildpath/"niv_python_lsp"
+    bin.install buildpath/"niv_nim_lsp"
+    bin.install buildpath/"niv_toml_lsp"
+    bin.install buildpath/"niv_yaml_lsp"
+    bin.install buildpath/"niv_md_lsp"
+    bin.install buildpath/"niv_bash_lsp"
+    bin.install buildpath/"niv_css_lsp"
+    bin.install buildpath/"niv_html_lsp"
   end
 
   test do
     assert_predicate bin/"niv", :executable?
     assert_predicate bin/"niv_json_lsp", :executable?
+    assert_predicate bin/"niv_python_lsp", :executable?
+    assert_predicate bin/"niv_nim_lsp", :executable?
+    assert_predicate bin/"niv_toml_lsp", :executable?
+    assert_predicate bin/"niv_yaml_lsp", :executable?
+    assert_predicate bin/"niv_md_lsp", :executable?
+    assert_predicate bin/"niv_bash_lsp", :executable?
+    assert_predicate bin/"niv_css_lsp", :executable?
+    assert_predicate bin/"niv_html_lsp", :executable?
   end
 end
