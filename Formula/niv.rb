caf5115dd902c1549cef7ba118e9204f5419d76a
akvilary <akvilary@gmail.com>
2026-03-23 23:23:30 +0800

    bump niv to 0.6.2

--- Formula/niv.rb
+++ Formula/niv.rb
@@ -2,8 +2,8 @@ class Niv < Formula
   desc "Vim-like terminal text editor with bundled LSP servers"
   homepage "https://github.com/akvilary/niv"
   url "https://github.com/akvilary/niv/archive/refs/heads/main.tar.gz"
-  sha256 "4bf0c2521a12a02afed3ea0939bac6a522cdede907fd098322d67924d7829756"
-  version "0.6.1"
+  sha256 "9bef53a4081532f1204b2f28f87f44283d61deb2001f4c80a0cbe4a57868dd14"
+  version "0.6.2"
   license "MIT"
 
   depends_on "gcc" => :build
